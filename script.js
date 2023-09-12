080022b4f1bf1b31787a6852b02688ae99d05501
sai-wunna <137637412+sai-wunna@users.noreply.github.com>
2023-09-12 11:55:23 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const getNode = (node) => document.querySelector(node)
+  const resultBox = getNode('.result')
+  let currentValue = ''
+
+  const keys = getNode('.keys')
+
+  keys.addEventListener('click', (e) => {
+    if (e.target.hasAttribute('value')) {
+      currentValue += e.target.getAttribute('value')
+      resultBox.textContent = currentValue
+    }
+    if (e.target.id === 'clear') {
+      currentValue = ''
+      resultBox.textContent = ''
+    }
+    if (e.target.id === 'ans') {
+      currentValue = eval(currentValue).toFixed(2).toString()
+      resultBox.textContent = currentValue
+      console.log(currentValue)
+    }
+    if (e.target.id === 'del') {
+      currentValue = currentValue.slice(0, -1)
+      resultBox.textContent = currentValue
+    }
+  })
+})
